b1984c3d95df94c6e72371535975a05efe474f4a
Kim Whitehall <kwhitehall@Kims-MacBook.local>
2015-09-03 23:35:30 -0700

    updated to reflect the (new) funcion calls in the other files

--- code/mainProgTemplate.py
+++ code/mainProgTemplate.py
@@ -20,12 +20,6 @@ def main():
     allCETRMMList =[]
     DIRS={}
 
-    # DIRS={
-    #          mainDirStr= "/directory/to/where/to/store/outputs"
-    #          TRMMdirName = "/directory/to/the/TRMM/netCDF/files"
-    #          CEoriDirName = "/directory/to/the/MERG/netCDF/files"
-    #         }
-
     #for GrADs
     subprocess.call('export DISPLAY=:0.0', shell=True)
 
@@ -33,13 +27,61 @@ def main():
     # rawMERG = "/directory/to/the/raw/MERGfiles"
     # utils.preprocessing_merg(rawMERG)
     # ---------------------------------------------------------------------------------
+    # ---------------------------------- user inputs --------------------------------------
+    DIRS['mainDirStr'] = "/Users/kwhitehall/Documents/capstones/usc2015/baselineTimings/test"#"/directory/to/where/to/store/outputs"
+    DIRS['TRMMdirName'] = "/Users/kwhitehall/Documents/capstones/usc2015/baselineTimings/datadir/TRMM"#"/directory/to/the/TRMM/netCDF/files"
+    DIRS['CEoriDirName'] = "/Users/kwhitehall/Documents/capstones/usc2015/baselineTimings/datadir/MERG"#"/directory/to/the/MERG/netCDF/files"
+    #get the dates for analysis
+    startDateTime = "200908310000" #"yyyymmddhrmm"
+    endDateTime = "200908312100"
+    # ---------------------------------- end user inputs --------------------------------------
+    # Checks that inputs are ok
+    try:
+        if not os.path.exists(DIRS['CEoriDirName']):
+            print "Error! MERG invalid path!"
+            DIRS['CEoriDirName'] = raw_input("> Please enter the directory to the MERG netCDF files: \n")
+    except:
+        print "..."
 
+    try:
+        if not os.path.exists(DIRS['TRMMdirName']):
+            print "Error: TRMM invalid path!"
+            DIRS['TRMMdirName'] = raw_input("> Please enter the location to the raw TRMM netCDF files: \n")
+    except:
+        pass
 
-    #create main directory and file structure for storing intel
-    DIRS['mainDirStr'] = utils.createMainDirectory(DIRS['mainDirStr'])
+    try:
+        if not os.path.exists(DIRS['CEoriDirName']):
+            print "Error! MERG invalid path!"
+            DIRS['CEoriDirName'] = raw_input("> Please enter the directory to the MERG netCDF files: \n")
+    except:
+        print "..."   
+
+    #check validity of time
+    while utils.valid_date(startDateTime) != True:
+        print "Invalid time entered for startDateTime!"
+
+    while utils.valid_date(endDateTime) != True:
+        print "Invalid time entered for endDateTime!"
+        
+    #check if all the files exisits in the MERG and TRMM directories entered
+    test,_ = iomethods.check_for_files(DIRS['TRMMdirName'], startDateTime, endDateTime, 3, 'hour')
+    if test == False:
+        print "Error with files in the TRMM directory entered. Please check your files before restarting. "
+        return
+    #test,filelist = iomethods.check_for_files(startDateTime, endDateTime, DIRS['CEoriDirName'],1)
+    test,filelist = iomethods.check_for_files(DIRS['CEoriDirName'], startDateTime, endDateTime, 1, 'hour')
+
+    if test == False:
+        print "Error with files in the original MERG directory entered. Please check your files before restarting. "
+        return
+    # end checks 
+
+    # create main directory and file structure for storing intel
+    DIRS['mainDirStr'] = iomethods.create_main_directory(DIRS['mainDirStr'])
     TRMMCEdirName = DIRS['mainDirStr']+'/TRMMnetcdfCEs'
     CEdirName = DIRS['mainDirStr']+'/MERGnetcdfCEs'
-
+    
     # for doing some postprocessing with the clipped datasets instead of running the full program, e.g.
     # mccSearch.post_processing_netcdf(3,CEoriDirName)
     # mccSearch.post_processing_netcdf(2)
@@ -47,11 +89,11 @@ def main():
 
     #let's go!
     print "\n -------------- Read MERG Data ----------"
-    mergImgs, timeList, LAT, LON = iomethods.readMergData(DIRS['CEoriDirName'], filelist)
+    mergImgs, timeList, LAT, LON = iomethods.read_data(DIRS['CEoriDirName'],'ch4','latitude','longitude', filelist)
     print ("-"*80)
 
     print "\n -------------- TESTING findCloudElements ----------"
-    CEGraph = mccSearch.findCloudElements(mergImgs,timeList,DIRS['mainDirStr'], LAT,LON,DIRS['TRMMdirName'])
+    CEGraph = mccSearch.find_cloud_elements(mergImgs,timeList,DIRS['mainDirStr'], LAT,LON,DIRS['TRMMdirName'])
     #if the TRMMdirName wasnt entered for whatever reason, you can still get the TRMM data this way
     # CEGraph = mccSearch.findCloudElements(mergImgs,timeList,DIRS['mainDirStr'], LAT,LON)
     # allCETRMMList=mccSearch.findPrecipRate(DIRS['TRMMdirName'],timeList)
@@ -60,13 +102,13 @@ def main():
     print "number of nodes in CEGraph is: ", CEGraph.number_of_nodes()
     print ("-"*80)
     print "\n -------------- TESTING findCloudClusters ----------"
-    prunedGraph = mccSearch.findCloudClusters(CEGraph)
+    prunedGraph = mccSearch.find_cloud_clusters(CEGraph)
     print ("-"*80)
     print "number of nodes in prunedGraph is: ", prunedGraph.number_of_nodes()
     print ("-"*80)
     #sys.exit()
     print "\n -------------- TESTING findMCCs ----------"
-    MCCList,MCSList = mccSearch.findMCC(prunedGraph)
+    MCCList,MCSList = mccSearch.find_MCC(prunedGraph)
     print ("-"*80)
     print "MCC List has been acquired ", len(MCCList)
     print "MCS List has been acquired ", len(MCSList)
