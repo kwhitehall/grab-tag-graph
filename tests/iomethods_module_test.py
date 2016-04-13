de54c8c572d6433fc0bd42d8c8ae57c5c7a5d5ff
Marcus Eng <marcuseng23@gmail.com>
2016-04-13 15:11:17 -0700

    change file type used for test

--- tests/iomethods_module_test.py
+++ tests/iomethods_module_test.py
@@ -13,7 +13,7 @@ def test_read_MERG_and_write_MERG():
     '''
     user = variables.UserVariables(useJSON=False)
 
-    lon, lat, temperatures = iomethods.read_MERG_pixel_file('../datadir/MERG/merg_2009083100_4km-pixel.nc')
+    lon, lat, temperatures = iomethods.read_MERG_pixel_file('../datadir/MERG/merg_2006091100_4km-pixel')
 
     lonDict = {"name": "longitude", "dataType": "double", "dimensions": ("longitude",), "units": "degrees_east", "long_name": "Longitude", "values": lon}
 
