1ebd274fbdbfe88f6d86aa85e28b17bb9c73f778
Campbell <caocampb@usc.edu>
2016-04-18 18:17:00 -0700

    Performance test for np.where etc. vs. for comprehension

--- tests/iomethods_module_test.py
+++ tests/iomethods_module_test.py
@@ -1,13 +1,13 @@
 import sys
+import timeit
 
 from datetime import datetime
-
-from code.iomethods import read_netCDF_to_array
 
 sys.path.insert(0, '../code/')
 
 import iomethods
 import variables
+import utils
 import numpy as np
 
 def test_read_merg_and_write_merg():
@@ -49,15 +49,15 @@ def test_read_netCDF_to_array():
     minDate = datetime(2005, 1, 1)
     maxDate = datetime(2007, 1, 1)
 
-    trimmedData, times, trimmedLats, trimmedLons = read_netCDF_to_array('/home/campbell/Desktop/TRMM Sample/3B42.20060911.00.7A.nc',
+    trimmedData, times, trimmedLats, trimmedLons = iomethods.read_netCDF_to_array('/home/caocampb/Desktop/TRMM Data/3B42.20060911.00.7A.nc',
                                                                            'trmm', 'irp', minDate, maxDate, 10, 15, 10, 15)
 
-    minDate = datetime(2009, 8, 21)
-    maxDate = datetime(2009, 8, 31)
-
-    trimmedData, times, trimmedLats, trimmedLons = read_netCDF_to_array(
-            '/Users/diegovonbeck/grab-tag-graph-bak/datadir/TRMM/3B42.20090831.00.7A.nc',
-             'trmm', 'irp', minDate, maxDate, 100, 15, 10, 15)
+    # minDate = datetime(2009, 8, 21)
+    # maxDate = datetime(2009, 8, 31)
+    #
+    # trimmedData, times, trimmedLats, trimmedLons = read_netCDF_to_array(
+    #         '/Users/diegovonbeck/grab-tag-graph-bak/datadir/TRMM/3B42.20090831.00.7A.nc',
+    #          'trmm', 'irp', minDate, maxDate, 100, 15, 10, 15)
 
     # Compare these with known values
     print trimmedData
@@ -65,9 +65,37 @@ def test_read_netCDF_to_array():
     print trimmedLats
     print trimmedLons
 
+def performanceTestDataSubset1():
+    lats = np.arange(0, 60., 0.05, dtype=np.float)
+
+    trimmedLatsIndices = [i for i in range(len(lats))
+                          if lats[i] <= 50 and lats[i] >= 30]
+
+    trimmedLatsStart = trimmedLatsIndices[0]
+    trimmedLatsEnd = trimmedLatsIndices[-1]
+
+    trimmedLats = lats[trimmedLatsEnd:trimmedLatsStart]
+
+
+def performanceTestDataSubset2():
+    lats = np.arange(0, 60., 0.05, dtype=np.float)
+
+    latminNETCDF = utils.find_nearest(lats, float(30))
+    latmaxNETCDF = utils.find_nearest(lats, float(50))
+
+    latminIndex = (np.where(lats == latminNETCDF))[0][0]
+    latmaxIndex = (np.where(lats == latmaxNETCDF))[0][0]
+
+    trimmedLats = lats[latminIndex:latmaxIndex]
+
 
 if __name__ == '__main__':
-    test_read_merg_and_write_merg()
-    test_read_netCDF_to_array()
+    start_time = timeit.default_timer()
+    performanceTestDataSubset1()
+    print(timeit.default_timer() - start_time)
+
+    start_time = timeit.default_timer()
+    performanceTestDataSubset2()
+    print(timeit.default_timer() - start_time)
 
 
